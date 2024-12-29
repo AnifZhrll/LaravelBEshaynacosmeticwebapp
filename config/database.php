625b7beb0a0b04ccac19f2e6639e6ed4acb28d0d
Muhammad Anif Zuhrul Anam <anamanif1@gmail.com>
2024-12-29 17:30:48 +0700

    minnor issue

--- config/database.php
+++ config/database.php
@@ -143,7 +143,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('phpredis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
@@ -151,12 +151,12 @@ return [
         ],
 
         'default' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
+            'url' => env(null),
+            'host' => env('127.0.0.1'),
+            'username' => env(null),
+            'password' => env(null),
+            'port' => env('6379'),
+            'database' => env('0'),
         ],
 
         'cache' => [
